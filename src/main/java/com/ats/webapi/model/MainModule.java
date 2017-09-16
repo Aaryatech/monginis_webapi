0198878ddd21c5279cabece91c0fbe33c1e7670e
Aaryatech <code@atsinfosoft.com>
2017-09-16 14:49:33 +0530

    main module comment //sachin

--- src/main/java/com/ats/webapi/model/MainModule.java
+++ src/main/java/com/ats/webapi/model/MainModule.java
@@ -12,7 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
+// sachin comments main module
 
 @Entity
 @Table(name = "m_module")
