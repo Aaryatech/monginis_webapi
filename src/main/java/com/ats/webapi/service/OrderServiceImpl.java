5dfdf563f14c0ce6ffd64c29033fbdf263355f74
MIRACLEINFOTAINMENT <MIRACLEINFOTAINMENT@192.168.1.5>
2017-09-21 15:56:36 +0530

    order changes //atul

--- src/main/java/com/ats/webapi/service/OrderServiceImpl.java
+++ src/main/java/com/ats/webapi/service/OrderServiceImpl.java
@@ -17,98 +17,101 @@ import com.ats.webapi.repository.OrdersRepository;
 
 @Service("orderService")
 public class OrderServiceImpl implements OrderService {
-	
-String jsonResult;
-@Autowired
-OrderRepository orderRepository;
-@Autowired
-ItemOrderHisRepository itemOrderHisRepository;
+
+	String jsonResult;
+	@Autowired
+	OrderRepository orderRepository;
+	@Autowired
+	ItemOrderHisRepository itemOrderHisRepository;
+
 	@Override
 	public List<Orders> placeOrder(List<Orders> list) {
-		List<Orders> returnList=new ArrayList();
-		for(Orders o : list) {
-			
-			
-			Orders prevOrder=orderRepository.findPreviousOrder(o.getItemId(), o.getFrId(), o.getOrderDate(), o.getMenuId());
-			
+		List<Orders> returnList = new ArrayList();
+		for (Orders o : list) {
+
+			Orders prevOrder = orderRepository.findPreviousOrder(o.getItemId(), o.getFrId(), o.getOrderDate(),
+					o.getMenuId());
+
 			try {
-				
-				System.out.println("prev order orderId "+prevOrder.getOrderId());
-				
-				
-					if(prevOrder.getItemId().equalsIgnoreCase("") || prevOrder.getItemId().equalsIgnoreCase(null)) {
-						
-					}else {
-						System.out.println("Updating order");
 
-						
+				System.out.println("prev order orderId " + prevOrder.getOrderId());
+
+				if (prevOrder.getItemId().equalsIgnoreCase("") || prevOrder.getItemId().equalsIgnoreCase(null)) {
+
+				} else {
+					System.out.println("Updating order with qty " + o.getOrderQty());
+
+					Orders updatedOrder = null;
+					if (o.getOrderQty() == 0) {
+
+						System.out.println("Deleteing order");
+
+						Long result = orderRepository.deleteByOrderDateAndMenuIdAndItemId(o.getOrderDate(),
+								o.getMenuId(), o.getItemId());
+
+						System.out.println("Order Deleted ? = " + result);
+					} else {
 						prevOrder.setOrderQty(o.getOrderQty());
-						Orders updatedOrder=orderRepository.save(prevOrder);
-
-						
-					 returnList.add(updatedOrder);
+						updatedOrder = orderRepository.save(prevOrder);
 					}
+
+					returnList.add(updatedOrder);
+				}
+
+			} catch (Exception e) {
+
+				System.out.println("Prev Order Exception " + e.getMessage());
 				
-				
-			}catch (Exception e) {
-				
-				System.out.println("Prev Order Exception "+e.getMessage());
-				
-				System.out.println("Saving new order");
-				
-				Orders newOrder=orderRepository.save(o);
-				
-				 returnList.add(newOrder);
-				
+				if (e.getMessage() == null) {
+					System.out.println("Saving new order");
+
+					Orders newOrder = orderRepository.save(o);
+					returnList.add(newOrder);
+				}
 			}
-			
-			
+
 			// Orders order=orderRepository.save(o);
 			// returnList.add(prevOrder);
-			 }
+		}
 
-       
 		return returnList;
 	}
+
 	@Override
-	public ItemOrderList searchOrderHistory(int menuId, Date deliveryDate,int frId) {
-		List<ItemOrderHis> orderList=null;
+	public ItemOrderList searchOrderHistory(int menuId, Date deliveryDate, int frId) {
+		List<ItemOrderHis> orderList = null;
 		ErrorMessage errorMessage;
 		ItemOrderList itemOrderList;
 		try {
-			orderList=itemOrderHisRepository.findByMenuIdInAndDeliveryDate(menuId, deliveryDate,frId);
-		
-			if(orderList==null)
-			{
-				errorMessage=new ErrorMessage();
-				itemOrderList=new ItemOrderList();
-				
+			orderList = itemOrderHisRepository.findByMenuIdInAndDeliveryDate(menuId, deliveryDate, frId);
+
+			if (orderList == null) {
+				errorMessage = new ErrorMessage();
+				itemOrderList = new ItemOrderList();
+
 				errorMessage.setError(true);
 				errorMessage.setMessage("Orders Not Found");
-				
-				
+
 				itemOrderList.setErrorMessage(errorMessage);
-			}
-			else
-			{
-				errorMessage=new ErrorMessage();
-				itemOrderList=new ItemOrderList();
+			} else {
+				errorMessage = new ErrorMessage();
+				itemOrderList = new ItemOrderList();
 
 				errorMessage.setError(false);
 				errorMessage.setMessage("Orders found Successfully");
-				
+
 				itemOrderList.setItemOrderList(orderList);
 				itemOrderList.setErrorMessage(errorMessage);
-				
+
 			}
-			
-		}catch (Exception e) {
-			itemOrderList=new ItemOrderList();
-			errorMessage=new ErrorMessage();
-			
+
+		} catch (Exception e) {
+			itemOrderList = new ItemOrderList();
+			errorMessage = new ErrorMessage();
+
 			errorMessage.setError(true);
 			errorMessage.setMessage("Orders Not Found(EXC)");
-			
+
 			itemOrderList.setErrorMessage(errorMessage);
 		}
 		return itemOrderList;
