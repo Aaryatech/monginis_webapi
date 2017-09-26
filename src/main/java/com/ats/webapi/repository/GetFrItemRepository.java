b47861d5e6c2f15a8b9decf70c49a43b332196c4
MIRACLEINFOTAINMENT <MIRACLEINFOTAINMENT@miracles-mini>
2017-09-26 11:39:56 +0530

    Delete status//atul

--- src/main/java/com/ats/webapi/repository/GetFrItemRepository.java
+++ src/main/java/com/ats/webapi/repository/GetFrItemRepository.java
@@ -11,7 +11,7 @@ import com.ats.webapi.model.ItemWithSubCat;
 public interface GetFrItemRepository extends  CrudRepository<ItemWithSubCat, Long>{
 		
 
-	 @Query(value = " SELECT m_item.* , m_cat_sub.sub_cat_name FROM m_item , m_cat_sub WHERE m_item.id IN(:items) AND m_cat_sub.sub_cat_id = m_item.item_grp2 ", nativeQuery = true)
+	 @Query(value = " SELECT m_item.* , m_cat_sub.sub_cat_name FROM m_item , m_cat_sub WHERE m_item.id IN(:items) AND m_cat_sub.sub_cat_id = m_item.item_grp2 AND m_item.del_status=0 ", nativeQuery = true)
 	    List<ItemWithSubCat> findFrItems(@Param ("items") List<Integer>items);
 	
 	
