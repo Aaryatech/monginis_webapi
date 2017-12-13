9935f1874d689651fde9c6cdae28fd28fe2a1584
ATS <ATS@192.168.2.3>
2017-12-13 19:12:15 +0530

    repository /akshay

--- src/main/java/com/ats/webapi/repository/PostProdPlanHeaderRepository.java
+++ src/main/java/com/ats/webapi/repository/PostProdPlanHeaderRepository.java
@@ -1,9 +1,11 @@
 package com.ats.webapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ats.webapi.model.PostProdPlanHeader;
 
@@ -14,5 +16,11 @@ public interface PostProdPlanHeaderRepository extends JpaRepository<PostProdPlan
 	@Query(value="SELECT production_header_id,production_date,cat_id,MAX(time_slot) as time_slot,production_batch,production_status,production_date,is_mixing,is_bom,del_status FROM t_production_plan_header  "
 			+ "WHERE production_date=:strDate And cat_id=:catId ",nativeQuery=true)
 	PostProdPlanHeader findTopTimeSlotByProductionDateAndCatId(@Param("strDate")String strDate,@Param("catId") int catId);
+	
+	
+	@Transactional
+	@Modifying
+	@Query(" UPDATE PostProdPlanHeader SET is_mixing=1 WHERE production_header_id=:productionId")
+	int updateisMixing(@Param("productionId")int productionId);
 
 }
