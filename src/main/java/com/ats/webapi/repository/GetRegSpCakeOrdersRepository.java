1314c35bfacda84156b8284d6c0a75c2bc783682
Aaryatech <code@atsinfosoft.com>
2018-01-13 18:45:27 +0530

    //Mahesh

--- src/main/java/com/ats/webapi/repository/GetRegSpCakeOrdersRepository.java
+++ src/main/java/com/ats/webapi/repository/GetRegSpCakeOrdersRepository.java
@@ -13,7 +13,7 @@ public interface GetRegSpCakeOrdersRepository extends JpaRepository<GetRegSpCake
 	
 	@Query(value="SELECT r.rsp_id, f.fr_name, f.fr_mob, t.item_name, r.order_date, r.mrp, r.qty, r.rsp_sub_total, r.rsp_advance_amt,"
 			+" r.rsp_remaining_amt, r.rsp_delivery_dt, r.rsp_cust_name, r.rsp_cust_mobile_no from t_regular_sp_cake r, m_item t, m_franchisee f where"
-			+" r.rsp_id IN (:orderNo) AND r.fr_id=f.fr_id AND r.item_id=t.id",nativeQuery=true)
+			+" r.rsp_id IN (:orderNo) AND  r.del_status=0 AND r.fr_id=f.fr_id AND r.item_id=t.id",nativeQuery=true)
 	
 	List<GetRegSpCakeOrders> getRegSpOrders(@Param("orderNo")List<String> orderNo);
 
