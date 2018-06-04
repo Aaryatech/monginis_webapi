bd0a890683f1f593e6197863ba56966970af55e4
ats-12 <ats-12@ats12>
2018-06-04 10:26:06 +0530

    //Mahesh

--- src/main/java/com/ats/webapi/repository/VarianceRepository.java
+++ src/main/java/com/ats/webapi/repository/VarianceRepository.java
@@ -11,14 +11,14 @@ import com.ats.webapi.model.Variance;
 public interface VarianceRepository extends JpaRepository<Variance, Integer>{
 
 	@Query(value="SELECT m_item.item_name, m_item.id, m_item.item_id, coalesce((Select sum(t_order.order_qty) From t_order "
-			+ "where t_order.production_date=:Date AND m_item.id=t_order.item_id ), 0) AS t_order_qty, "
+			+ "where t_order.production_date=:Date and t_order.is_bill_generated=0  AND m_item.id=t_order.item_id ), 0) AS t_order_qty, "
 			+ "coalesce((Select SUM( t_regular_sp_cake.qty) From t_regular_sp_cake where m_item.id= t_regular_sp_cake.item_id "
 			+ "AND t_regular_sp_cake.rsp_produ_date=:Date), 0) AS sp_cake_qty from m_item WHERE m_item.item_grp1=:group_type "
 			+ "Group by id",nativeQuery=true)
 	List<Variance> variancelistAllFr(@Param("Date")String date,@Param("group_type")String groupType);
 	
 	@Query(value="SELECT m_item.item_name, m_item.id, m_item.item_id, coalesce((Select sum(t_order.order_qty) From t_order "
-			+ "where t_order.production_date=:Date AND m_item.id=t_order.item_id AND t_order.fr_id IN(:frId)), 0) AS t_order_qty,"
+			+ "where t_order.production_date=:Date and t_order.is_bill_generated=0 AND m_item.id=t_order.item_id AND t_order.fr_id IN(:frId)), 0) AS t_order_qty,"
 			+ " coalesce((Select SUM( t_regular_sp_cake.qty) From t_regular_sp_cake where m_item.id= t_regular_sp_cake.item_id"
 			+ " AND t_regular_sp_cake.rsp_produ_date=:Date AND t_regular_sp_cake.fr_id in(:frId) ), 0) AS sp_cake_qty "
 			+ "from m_item WHERE m_item.item_grp1=:group_type Group by id",nativeQuery=true)
