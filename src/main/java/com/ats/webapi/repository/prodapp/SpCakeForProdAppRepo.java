f8214da2c811ffbf00f3bf2942fafff9d17db4ed
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-06-19 10:14:53 +0530

    //..neha

--- src/main/java/com/ats/webapi/repository/prodapp/SpCakeForProdAppRepo.java
+++ src/main/java/com/ats/webapi/repository/prodapp/SpCakeForProdAppRepo.java
@@ -21,11 +21,11 @@ public interface SpCakeForProdAppRepo extends JpaRepository<GetSpCakeOrderForPro
 			+ "AND t_sp_cake.sp_order_no=t_sp_cake_sup.t_sp_cake_order_no AND t_sp_cake_sup.fr_id=m_franchisee.fr_id\n"
 			+ "AND m_franchisee.fr_id=m_franchise_sup.fr_id AND m_franchisee.fr_route_id=m_fr_route.route_id\n"
 			+ "AND m_sp_cake.sp_id=t_sp_cake_sup.sp_cake_id AND m_sp_flavour.spf_id=t_sp_cake.sp_flavour_id\n"
-			+ "ORDER BY m_fr_route.route_seq_no DESC,m_franchise_sup.no_in_route,t_sp_cake_sup.fr_id ", nativeQuery = true)
+			+ "ORDER BY m_fr_route.route_seq_no DESC,m_franchise_sup.no_in_route,t_sp_cake_sup.fr_id GROUP BY t_sp_cake_sup.t_sp_cake_order_no ", nativeQuery = true)
 
 	List<GetSpCakeOrderForProdApp> getSpCakeOrderForProdOrderBy(@Param("fromDate") String fromDate,
 			@Param("toDate") String toDate, @Param("menuIdList") List<Integer> menuIdList,
-			@Param("isSlotUsed")  List<Integer> isSlotUsed);//D] specific menu order by
+			@Param("isSlotUsed") List<Integer> isSlotUsed);// D] specific menu order by
 
 	@Query(value = "SELECT t_sp_cake_sup.sr_no,t_sp_cake_sup.t_sp_cake_sup_no,t_sp_cake_sup.date,t_sp_cake_sup.status,t_sp_cake_sup.is_char_used,\n"
 			+ "t_sp_cake_sup.input_kg_fr,m_fr_route.route_name,m_franchise_sup.no_in_route, t_sp_cake_sup.input_kg_prod,\n"
@@ -38,10 +38,10 @@ public interface SpCakeForProdAppRepo extends JpaRepository<GetSpCakeOrderForPro
 			+ "AND t_sp_cake.sp_order_no=t_sp_cake_sup.t_sp_cake_order_no AND t_sp_cake_sup.fr_id=m_franchisee.fr_id "
 			+ "AND m_franchisee.fr_id=m_franchise_sup.fr_id AND m_franchisee.fr_route_id=m_fr_route.route_id\n"
 			+ "AND m_sp_cake.sp_id=t_sp_cake_sup.sp_cake_id AND m_sp_flavour.spf_id=t_sp_cake.sp_flavour_id\n"
-			+ "ORDER BY m_fr_route.route_seq_no DESC,m_franchise_sup.no_in_route,t_sp_cake_sup.fr_id ", nativeQuery = true)
+			+ "ORDER BY m_fr_route.route_seq_no DESC,m_franchise_sup.no_in_route,t_sp_cake_sup.fr_id  GROUP BY t_sp_cake_sup.t_sp_cake_order_no ", nativeQuery = true)
 
 	List<GetSpCakeOrderForProdApp> getSpCakeOrderForProdOrderByAndAllMenu(@Param("fromDate") String fromDate,
-			@Param("toDate") String toDate, @Param("isSlotUsed") List<Integer> isSlotUsed);//B] all menu order by 
+			@Param("toDate") String toDate, @Param("isSlotUsed") List<Integer> isSlotUsed);// B] all menu order by
 
 	@Query(value = "SELECT t_sp_cake_sup.sr_no,t_sp_cake_sup.t_sp_cake_sup_no,t_sp_cake_sup.date,t_sp_cake_sup.status,t_sp_cake_sup.is_char_used,\n"
 			+ "t_sp_cake_sup.input_kg_fr,m_fr_route.route_name,m_franchise_sup.no_in_route,t_sp_cake_sup.input_kg_prod, \n"
@@ -53,11 +53,12 @@ public interface SpCakeForProdAppRepo extends JpaRepository<GetSpCakeOrderForPro
 			+ "WHERE t_sp_cake_sup.date BETWEEN :fromDate AND :toDate AND t_sp_cake_sup.menu_id IN (:menuIdList) AND t_sp_cake.is_slot_used IN (:isSlotUsed) "
 			+ "AND t_sp_cake.sp_order_no=t_sp_cake_sup.t_sp_cake_order_no AND t_sp_cake_sup.fr_id=m_franchisee.fr_id\n"
 			+ "AND m_franchisee.fr_id=m_franchise_sup.fr_id AND m_franchisee.fr_route_id=m_fr_route.route_id\n"
-			+ "AND m_sp_cake.sp_id=t_sp_cake_sup.sp_cake_id AND m_sp_flavour.spf_id=t_sp_cake.sp_flavour_id\n"
+			+ "AND m_sp_cake.sp_id=t_sp_cake_sup.sp_cake_id AND m_sp_flavour.spf_id=t_sp_cake.sp_flavour_id GROUP BY t_sp_cake_sup.t_sp_cake_order_no \n"
 			+ "", nativeQuery = true)
 
-	List<GetSpCakeOrderForProdApp> getSpCakeOrderForProdGen(@Param("fromDate") String fromDate, @Param("toDate") String toDate,
-			@Param("menuIdList") List<Integer> menuIdList, @Param("isSlotUsed") List<Integer> isSlotUsed); //C] specific menu no order by 
+	List<GetSpCakeOrderForProdApp> getSpCakeOrderForProdGen(@Param("fromDate") String fromDate,
+			@Param("toDate") String toDate, @Param("menuIdList") List<Integer> menuIdList,
+			@Param("isSlotUsed") List<Integer> isSlotUsed); // C] specific menu no order by
 
 	@Query(value = "SELECT t_sp_cake_sup.sr_no,t_sp_cake_sup.t_sp_cake_sup_no,t_sp_cake_sup.date,t_sp_cake_sup.status, t_sp_cake_sup.is_char_used, \n"
 			+ "t_sp_cake_sup.input_kg_fr,m_fr_route.route_name,m_franchise_sup.no_in_route, t_sp_cake_sup.input_kg_prod,\n"
@@ -69,13 +70,14 @@ public interface SpCakeForProdAppRepo extends JpaRepository<GetSpCakeOrderForPro
 			+ "WHERE t_sp_cake_sup.date BETWEEN :fromDate AND :toDate AND t_sp_cake.is_slot_used IN (:isSlotUsed) "
 			+ "AND t_sp_cake.sp_order_no=t_sp_cake_sup.t_sp_cake_order_no AND t_sp_cake_sup.fr_id=m_franchisee.fr_id\n"
 			+ "AND m_franchisee.fr_id=m_franchise_sup.fr_id AND m_franchisee.fr_route_id=m_fr_route.route_id\n"
-			+ "AND m_sp_cake.sp_id=t_sp_cake_sup.sp_cake_id AND m_sp_flavour.spf_id=t_sp_cake.sp_flavour_id\n"
+			+ "AND m_sp_cake.sp_id=t_sp_cake_sup.sp_cake_id AND m_sp_flavour.spf_id=t_sp_cake.sp_flavour_id GROUP BY t_sp_cake_sup.t_sp_cake_order_no \n"
 			+ " ", nativeQuery = true)
 
 	List<GetSpCakeOrderForProdApp> getSpCakeOrderForProdGenAllMenu(@Param("fromDate") String fromDate,
-			@Param("toDate") String toDate, @Param("isSlotUsed") List<Integer> isSlotUsed);//A onload all menu no order by 
-	
-	//these two to consume
+			@Param("toDate") String toDate, @Param("isSlotUsed") List<Integer> isSlotUsed);// A onload all menu no order
+																							// by
+
+	// these two to consume
 	@Query(value = "SELECT t_sp_cake_sup.sr_no,t_sp_cake_sup.t_sp_cake_sup_no,t_sp_cake_sup.date,t_sp_cake_sup.status,t_sp_cake_sup.is_char_used,\n"
 			+ "t_sp_cake_sup.input_kg_fr,m_fr_route.route_name,m_franchise_sup.no_in_route,t_sp_cake_sup.input_kg_prod,\n"
 			+ "m_franchisee.fr_name,m_franchisee.fr_code,m_franchisee.fr_id,m_fr_route.route_id,\n"
@@ -90,8 +92,9 @@ public interface SpCakeForProdAppRepo extends JpaRepository<GetSpCakeOrderForPro
 			+ "ORDER BY m_fr_route.route_seq_no DESC,m_franchise_sup.no_in_route,t_sp_cake_sup.fr_id ", nativeQuery = true)
 
 	List<GetSpCakeOrderForProdApp> getSpCakeOrderByRouteIdSpecMenu(@Param("fromDate") String fromDate,
-			@Param("toDate") String toDate, @Param("menuIdList") List<Integer> menuIdList,@Param("routeId") int routeId);// specific menu order by
-	
+			@Param("toDate") String toDate, @Param("menuIdList") List<Integer> menuIdList,
+			@Param("routeId") int routeId);// specific menu order by
+
 	@Query(value = "SELECT t_sp_cake_sup.sr_no,t_sp_cake_sup.t_sp_cake_sup_no,t_sp_cake_sup.date,t_sp_cake_sup.status,t_sp_cake_sup.is_char_used,\n"
 			+ "t_sp_cake_sup.input_kg_fr,m_fr_route.route_name,m_franchise_sup.no_in_route,t_sp_cake_sup.input_kg_prod,\n"
 			+ "m_franchisee.fr_name,m_franchisee.fr_code,m_franchisee.fr_id,m_fr_route.route_id,\n"
@@ -106,8 +109,6 @@ public interface SpCakeForProdAppRepo extends JpaRepository<GetSpCakeOrderForPro
 			+ "ORDER BY m_fr_route.route_seq_no DESC,m_franchise_sup.no_in_route,t_sp_cake_sup.fr_id ", nativeQuery = true)
 
 	List<GetSpCakeOrderForProdApp> getSpCakeOrderByRouteIdAllMenu(@Param("fromDate") String fromDate,
-			@Param("toDate") String toDate,@Param("routeId") int routeId);// specific menu order by
-
-
+			@Param("toDate") String toDate, @Param("routeId") int routeId);// specific menu order by
 
 }
