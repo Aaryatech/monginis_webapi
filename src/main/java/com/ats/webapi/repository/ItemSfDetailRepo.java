82463bbf2486b499f899e9c4626d4e9da7730d9d
ats-12 <ats-12@ats12>
2019-09-24 13:01:01 +0530

    //Mahesh

--- src/main/java/com/ats/webapi/repository/ItemSfDetailRepo.java
+++ src/main/java/com/ats/webapi/repository/ItemSfDetailRepo.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.ats.webapi.model.rawmaterial.ItemSfDetail;
-import com.ats.webapi.model.rawmaterial.SfItemDetailList;
 
 public interface ItemSfDetailRepo extends JpaRepository<ItemSfDetail, Integer>{
 	
@@ -15,7 +14,23 @@ public interface ItemSfDetailRepo extends JpaRepository<ItemSfDetail, Integer>{
 	
 	List<ItemSfDetail> findByDelStatusAndSfId(int delStatus,int sfId);
 
-	@Query(value="",nativeQuery=true)
-	public ItemSfDetail getSfItemDetailsForCreamPrep(@Param("sfQty")float sfQty,@Param("sfId") int sfId);
+
+	@Query(value="    SELECT\n" + 
+			"        m_item_sf_detail.sf_did,\n" + 
+			"        m_item_sf_detail.sf_id,\n" + 
+			"         m_item_sf_detail.rm_id,\n" + 
+			"        concat(m_item_sf_detail.rm_name,'#',m_rm_uom.uom) as rm_name,\n" + 
+			"        m_item_sf_detail.rm_type,\n" + 
+			"        m_item_sf_detail.rm_qty,\n" + 
+			"        m_item_sf_detail.rm_weight,\n" + 
+			"        m_item_sf_detail.del_status,\n" + 
+			"        m_item_sf_detail.rm_unit\n" + 
+			"    FROM\n" + 
+			"        m_item_sf_detail,m_rm_uom" + 
+			"    WHERE\n" + 
+			"        m_item_sf_detail.sf_id in(:sfId) and m_item_sf_detail.del_status=0 and m_rm_uom.uom=m_item_sf_detail.rm_unit" + 
+			"    GROUP by\n" + 
+			"        m_item_sf_detail.rm_id",nativeQuery=true)
+	public List<ItemSfDetail> getSfItemDetailsForCreamPrep(@Param("sfId")List<Integer> sfId);
 
 }
