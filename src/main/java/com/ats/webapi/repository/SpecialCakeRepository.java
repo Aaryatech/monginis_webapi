052d3f88f2357b3b20b2ff33f463d34a30461336
Aaryatech <code@atsinfosoft.com>
2019-09-27 17:18:04 +0530

    //Anmol

--- src/main/java/com/ats/webapi/repository/SpecialCakeRepository.java
+++ src/main/java/com/ats/webapi/repository/SpecialCakeRepository.java
@@ -22,7 +22,7 @@ public interface SpecialCakeRepository extends JpaRepository<SpecialCake, Intege
 			+ "FROM m_sp_cake sc ,t_sp_cake_album a WHERE sc.del_status=0 AND a.del_status=0 AND a.is_active=1 AND a.sp_id=sc.sp_id", nativeQuery = true)
 	List<SpecialCake> getData();
 
-	@Query(value = "SELECT a.album_id AS sp_id, a.album_code AS sp_code, a.album_name AS sp_name,"
+	/*@Query(value = "SELECT a.album_id AS sp_id, a.album_code AS sp_code, a.album_name AS sp_name,"
 			+ " sc.sp_type, a.min_wt AS sp_min_wt, a.max_wt AS sp_max_wt, sc.sp_book_b4, "
 			+ "sc.spr_id, a.photo1 AS sp_image, sc.sp_tax1, sc.sp_tax2, sc.sp_tax3, "
 			+ "sc.spe_id_list, sc.is_used, sc.sp_pho_upload, sc.time_two_appli, "
@@ -35,6 +35,19 @@ public interface SpecialCakeRepository extends JpaRepository<SpecialCake, Intege
 			+ "FROM m_fr_configure fc,m_fr_menu_show m WHERE fc.menu_id=m.menu_id AND "
 			+ "fc.is_del=0 AND m.del_status=0 AND fc.fr_id=:frId AND m.cat_id=5 AND "
 			+ "m.is_same_day_applicable=4))", nativeQuery = true)
+	List<SpecialCake> getDataByFr(@Param("frId")int frId);*/
+	
+	
+	@Query(value = "SELECT a.album_id AS sp_id, a.album_code AS sp_code, a.album_name AS sp_name,"
+			+ " sc.sp_type, a.min_wt AS sp_min_wt, a.max_wt AS sp_max_wt, sc.sp_book_b4, "
+			+ "sc.spr_id, a.photo1 AS sp_image, sc.sp_tax1, sc.sp_tax2, sc.sp_tax3, "
+			+ "sc.spe_id_list, sc.is_used, sc.sp_pho_upload, sc.time_two_appli, "
+			+ "sc.del_status, sc.base_code, sc.sp_desc, sc.order_qty, sc.order_discount, "
+			+ "sc.is_cust_choice_ck, sc.is_addon_rate_appli, sc.mrp_rate1, sc.mrp_rate2, "
+			+ "sc.mrp_rate3, sc.sp_rate1, sc.sp_rate2, sc.sp_rate3, sc.is_slot_used, "
+			+ "sc.sp_code AS erp_link_code FROM m_sp_cake_category sc, t_sp_cake_album a "
+			+ "WHERE sc.del_status = 0 AND a.del_status = 0 AND a.is_active = 1 AND "
+			+ "a.sp_id = sc.sp_id AND a.is_visible_to_album=0 AND FIND_IN_SET(a.album_id,(SELECT GROUP_CONCAT(fc.item_show) FROM m_fr_configure fc, m_fr_menu_show m WHERE fc.fr_id = :frId AND m.cat_id = 5 AND m.is_same_day_applicable = 4 ))", nativeQuery = true)
 	List<SpecialCake> getDataByFr(@Param("frId")int frId);
 	
 	
