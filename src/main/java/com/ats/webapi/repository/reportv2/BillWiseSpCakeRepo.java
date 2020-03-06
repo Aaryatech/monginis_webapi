49132dca0559bac3cb10343a1dfb867227ba16e2
maddy <maddy@maddy-Inspiron-3541>
2020-03-06 13:57:53 +0530

    //maddy

--- src/main/java/com/ats/webapi/repository/reportv2/BillWiseSpCakeRepo.java
+++ src/main/java/com/ats/webapi/repository/reportv2/BillWiseSpCakeRepo.java
@@ -61,51 +61,72 @@ public interface BillWiseSpCakeRepo extends JpaRepository<GetBillWiseSpCakeRep, 
 	
 	/*********************************************************************************************************/
 	
-	@Query(value="SELECT \n" + 
-			"        UUID() AS id, h.invoice_no, DATE_FORMAT(h.bill_date, '%d-%m-%Y') AS bill_date, \n" + 
-			"        d.bill_no, \n" + 
-			"        SUM(d.bill_qty) AS qty, \n" + 
-			"        SUM(d.grand_total) AS grand_total, \n" + 
-			"        d.item_id, \n" + 
-			"        h.fr_id, \n" + 
-			"        CASE WHEN s.is_allocated = 0 THEN s.item_id ELSE SUBSTRING_INDEX(s.item_id, '#', -1) \n" + 
-			"             END AS item_name \n" + 
-			"FROM \n" + 
-			"        t_bill_header h, \n" + 
-			"        t_bill_detail d, \n" + 
-			"        t_sp_cake s \n" + 
-			"WHERE \n" + 
-			"        d.cat_id = 5 AND h.del_status = 0 AND h.bill_no = d.bill_no AND s.sp_order_no = d.order_id AND  \n" + 
-			"        h.bill_date BETWEEN :fromDate AND :toDate\n" + 
-			"GROUP BY \n" + 
-			"        d.item_id, \n" + 
-			"        d.bill_no\n" + 
-			"ORDER BY \n" + 
-			"        h.invoice_no", nativeQuery=true)
+	@Query(value="SELECT\n" + 
+			"        UUID() AS id,       \n" + 
+			"        h.invoice_no,\n" + 
+			"        DATE_FORMAT(h.bill_date,\n" + 
+			"        '%d-%m-%Y') AS bill_date,\n" + 
+			"        d.bill_no,\n" + 
+			"        SUM(d.bill_qty) AS qty,\n" + 
+			"        SUM(d.grand_total) AS grand_total,\n" + 
+			"        d.item_id,\n" + 
+			"        h.fr_id,\n" + 
+			"        CASE\n" + 
+			"            WHEN s.is_allocated = 0 THEN s.item_id\n" + 
+			"            ELSE SUBSTRING_INDEX(s.item_id,\n" + 
+			"            '#',\n" + 
+			"            -1)              \n" + 
+			"        END AS item_name  \n" + 
+			"    FROM\n" + 
+			"        t_bill_header h,\n" + 
+			"        t_bill_detail d,\n" + 
+			"        t_sp_cake s       \n" + 
+			"    WHERE\n" + 
+			"        d.cat_id = 5\n" + 
+			"        AND h.del_status = 0\n" + 
+			"        AND h.bill_no = d.bill_no\n" + 
+			"        AND s.sp_order_no = d.order_id\n" + 
+			"        AND h.bill_date BETWEEN :fromDate AND :toDate\n" + 
+			"    GROUP BY\n" + 
+			"        h.fr_id,\n" + 
+			"        d.item_id       \n" + 
+			"    ORDER BY\n" + 
+			"        h.fr_id", nativeQuery=true)
 	public List<GetBillWiseSpCakeRep> getAllSpCakeDetailGrpInvoice(@Param("fromDate") String fromDate, @Param("toDate") String toDate);
 
 	
-	@Query(value="SELECT \n" + 
-			"        UUID() AS id, h.invoice_no, DATE_FORMAT(h.bill_date, '%d-%m-%Y') AS bill_date, \n" + 
-			"        d.bill_no, \n" + 
-			"        SUM(d.bill_qty) AS qty, \n" + 
-			"        SUM(d.grand_total) AS grand_total, \n" + 
-			"        d.item_id, \n" + 
-			"        h.fr_id, \n" + 
-			"        CASE WHEN s.is_allocated = 0 THEN s.item_id ELSE SUBSTRING_INDEX(s.item_id, '#', -1) \n" + 
-			"             END AS item_name \n" + 
-			"FROM \n" + 
-			"        t_bill_header h, \n" + 
-			"        t_bill_detail d, \n" + 
-			"        t_sp_cake s \n" + 
-			"WHERE \n" + 
-			"        d.cat_id = 5 AND h.del_status = 0 AND h.bill_no = d.bill_no AND s.sp_order_no = d.order_id AND  \n" + 
-			"        h.bill_date BETWEEN :fromDate AND :toDate AND h.fr_id IN (:frIdList)\n" + 
-			"GROUP BY \n" + 
-			"        d.item_id, \n" + 
-			"        d.bill_no\n" + 
-			"ORDER BY \n" + 
-			"        h.invoice_no", nativeQuery=true)
+	@Query(value="SELECT\n" + 
+			"        UUID() AS id,\n" + 
+			"        h.invoice_no,\n" + 
+			"        DATE_FORMAT(h.bill_date,\n" + 
+			"        '%d-%m-%Y') AS bill_date,\n" + 
+			"        d.bill_no,\n" + 
+			"        SUM(d.bill_qty) AS qty,\n" + 
+			"        SUM(d.grand_total) AS grand_total,\n" + 
+			"        d.item_id,\n" + 
+			"        h.fr_id,\n" + 
+			"        CASE\n" + 
+			"            WHEN s.is_allocated = 0 THEN s.item_id\n" + 
+			"            ELSE SUBSTRING_INDEX(s.item_id,\n" + 
+			"            '#',\n" + 
+			"            -1)              \n" + 
+			"        END AS item_name  \n" + 
+			"    FROM\n" + 
+			"        t_bill_header h,\n" + 
+			"        t_bill_detail d,\n" + 
+			"        t_sp_cake s       \n" + 
+			"    WHERE\n" + 
+			"        d.cat_id = 5\n" + 
+			"        AND h.del_status = 0\n" + 
+			"        AND h.bill_no = d.bill_no\n" + 
+			"        AND s.sp_order_no = d.order_id\n" + 
+			"        AND h.bill_date BETWEEN :fromDate AND :toDate \n" + 
+			"        AND h.fr_id IN(:frIdList)\n" + 
+			"    GROUP BY\n" + 
+			"    h.fr_id,\n" + 
+			"        d.item_id       \n" + 
+			"    ORDER BY\n" + 
+			"      h.fr_id", nativeQuery=true)
 	public List<GetBillWiseSpCakeRep> getSpCakeDetailGrpInvoice(@Param("frIdList")List<String> frIdList, 
 			@Param("fromDate") String fromDate,@Param("toDate") String toDate);
 	
