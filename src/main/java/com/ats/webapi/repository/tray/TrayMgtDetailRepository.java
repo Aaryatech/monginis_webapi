314e6419350c1fbe9e5ac42fba2af90a95570d21
Aaryatech <code@atsinfosoft.com>
2020-10-09 17:33:19 +0530

    Anmol

--- src/main/java/com/ats/webapi/repository/tray/TrayMgtDetailRepository.java
+++ src/main/java/com/ats/webapi/repository/tray/TrayMgtDetailRepository.java
@@ -53,6 +53,100 @@ public interface TrayMgtDetailRepository extends JpaRepository<TrayMgtDetail, In
     @Query(value = " select t.tran_detail_id,t.tran_id,t.fr_id,t.outtray_date,t.outtray_big,t.outtray_small,t.outtray_lead,t.outtray_extra,t.intray_date,t.intray_big,t.intray_small,t.intray_lead,t.intray_extra,t.intray_date1,t.intray_big1,t.intray_small1,t.intray_lead1,t.intray_extra1,SUM(t.balance_big) AS balance_big,SUM(t.balance_small) AS balance_small,SUM(t.balance_lead) AS balance_lead,SUM(t.balance_extra) AS balance_extra,t.intray_big2,t.intray_small2,t.intray_lead2,t.intray_extra2,t.price_big,t.price_small,t.price_lead,t.price_extra,t.grand_total,t.tray_status,t.deposit_is_used,t.del_status,t.intray_date2,t.qty_big,t.qty_small,t.qty_lead,t.qty_extra,t.gst_per,t.gst_rs,t.taxable_amt,t.tax_amt,t.is_same_day,f.fr_name from t_tray_mgt_detail t,m_franchisee f where t.fr_id=f.fr_id and t.fr_id=:frId  And t.del_status=:delStatus GROUP BY t.fr_id", nativeQuery = true)
 	List<TrayMgtDetail> findByFrIdAndDelStatus(@Param("frId") int frId, @Param("delStatus") int delStatus);
 
+    
+    @Query(value = " SELECT\n" + 
+    		"    t.tran_detail_id,\n" + 
+    		"    t.tran_id,\n" + 
+    		"    t.fr_id,\n" + 
+    		"    t.outtray_date,\n" + 
+    		"    t.outtray_big,\n" + 
+    		"    t.outtray_small,\n" + 
+    		"    t.outtray_lead,\n" + 
+    		"    t.outtray_extra,\n" + 
+    		"    t.intray_date,\n" + 
+    		"    t.intray_big,\n" + 
+    		"    t.intray_small,\n" + 
+    		"    t.intray_lead,\n" + 
+    		"    t.intray_extra,\n" + 
+    		"    t.intray_date1,\n" + 
+    		"    t.intray_big1,\n" + 
+    		"    t.intray_small1,\n" + 
+    		"    t.intray_lead1,\n" + 
+    		"    t.intray_extra1,\n" + 
+    		"    COALESCE((t1.out_big - t2.in_big),\n" + 
+    		"    0) AS balance_big,\n" + 
+    		"    COALESCE((t1.out_small - t2.in_small),\n" + 
+    		"    0) AS balance_small,\n" + 
+    		"    COALESCE((t1.out_lead - t2.in_lead),\n" + 
+    		"    0) AS balance_lead,\n" + 
+    		"    0 AS balance_extra,\n" + 
+    		"    t.intray_big2,\n" + 
+    		"    t.intray_small2,\n" + 
+    		"    t.intray_lead2,\n" + 
+    		"    t.intray_extra2,\n" + 
+    		"    t.price_big,\n" + 
+    		"    t.price_small,\n" + 
+    		"    t.price_lead,\n" + 
+    		"    t.price_extra,\n" + 
+    		"    t.grand_total,\n" + 
+    		"    t.tray_status,\n" + 
+    		"    t.deposit_is_used,\n" + 
+    		"    t.del_status,\n" + 
+    		"    t.intray_date2,\n" + 
+    		"    t.qty_big,\n" + 
+    		"    t.qty_small,\n" + 
+    		"    t.qty_lead,\n" + 
+    		"    t.qty_extra,\n" + 
+    		"    t.gst_per,\n" + 
+    		"    t.gst_rs,\n" + 
+    		"    t.taxable_amt,\n" + 
+    		"    t.tax_amt,\n" + 
+    		"    t.is_same_day,\n" + 
+    		"    t.fr_name\n" + 
+    		"FROM\n" + 
+    		"    (\n" + 
+    		"    SELECT\n" + 
+    		"        1 AS flg,\n" + 
+    		"        d.*,\n" + 
+    		"        f.fr_name\n" + 
+    		"    FROM\n" + 
+    		"        t_tray_mgt_detail d,\n" + 
+    		"        m_franchisee f\n" + 
+    		"    WHERE\n" + 
+    		"        d.fr_id = f.fr_id AND d.fr_id = :frId AND d.del_status = 0\n" + 
+    		"    LIMIT 1\n" + 
+    		") t\n" + 
+    		"LEFT JOIN(\n" + 
+    		"    SELECT 1 AS flg,\n" + 
+    		"                COALESCE(SUM(d.outtray_small),0) AS out_small,\n" + 
+    		"        COALESCE(SUM(d.outtray_big),0) AS out_big,\n" + 
+    		"        COALESCE(SUM(d.outtray_lead),0) AS out_lead\n" + 
+    		"    FROM\n" + 
+    		"        t_tray_mgt_header h,\n" + 
+    		"        t_tray_mgt_detail d\n" + 
+    		"    WHERE\n" + 
+    		"        h.del_status = 0 AND d.del_status = 0 AND h.tran_id = d.tran_id AND d.fr_id = :frId\n" + 
+    		") t1\n" + 
+    		"ON\n" + 
+    		"    t.flg = t1.flg\n" + 
+    		"LEFT JOIN(\n" + 
+    		"    SELECT\n" + 
+    		"        1 AS flg,\n" + 
+    		"         COALESCE(SUM(i.intray_small),0) AS in_small,\n" + 
+    		"        COALESCE(SUM(i.intray_big),0) AS in_big,\n" + 
+    		"        COALESCE(SUM(i.intray_lead),0) AS in_lead" + 
+    		"    FROM\n" + 
+    		"        t_tray_mgt_header h,\n" + 
+    		"        t_tray_mgt_detail d,\n" + 
+    		"        t_tray_mgmt_detail_intray i\n" + 
+    		"    WHERE\n" + 
+    		"        h.del_status = 0 AND d.del_status = 0 AND i.del_status = 0 AND h.tran_id = i.tran_id AND d.tran_detail_id = i.tran_detail_id AND i.fr_id = :frId\n" + 
+    		") t2\n" + 
+    		"ON\n" + 
+    		"    t1.flg = t2.flg", nativeQuery = true)
+   	List<TrayMgtDetail> getBalByFr(@Param("frId") int frId);
+    
+    
     @Query(value="SELECT\n" + 
     		"    t1.tran_detail_id,\n" + 
     		"    t1.tran_id,\n" + 
--- src/main/java/com/ats/webapi/service/tray/TrayMgtServiceImpl.java
+++ src/main/java/com/ats/webapi/service/tray/TrayMgtServiceImpl.java
@@ -29,46 +29,52 @@ import com.ats.webapi.repository.tray.TrayMgtDetailRepository;
 import com.ats.webapi.repository.tray.TrayMgtHeaderRepository;
 
 @Service
-public class TrayMgtServiceImpl implements TrayMgtService{
+public class TrayMgtServiceImpl implements TrayMgtService {
 
 	@Autowired
 	TrayMgtHeaderRepository trayMgtHeaderRepository;
-	
+
 	@Autowired
 	TrayMgtDetailRepository trayMgtDetailRepository;
-	
+
 	@Autowired
 	FrOutTrayRepository frOutTraysRepository;
-	
+
 	@Autowired
 	GetTrayMgtHeaderRepository getTrayMgtHeaderRepository;
-	
+
 	@Autowired
 	TrayMgtDetailBeanRepository trayMgtDetailBeanRepository;
-	
+
 	@Autowired
 	FranchiseInRouteRepository franchiseInRouteRepository;
-	
+
 	@Autowired
 	GetTrayMgtReportRepo getTrayMgtReportRepo;
-	
+
 	@Autowired
 	GetInTraysRepository getInTraysRepository;
-	
-	
+
 	@Override
 	public TrayMgtHeader saveTrayMgtHeader(TrayMgtHeader trayMgtHeader) {
 
-		TrayMgtHeader trayMgtHeaderRes=trayMgtHeaderRepository.saveAndFlush(trayMgtHeader);
-		
+		TrayMgtHeader trayMgtHeaderRes = trayMgtHeaderRepository.saveAndFlush(trayMgtHeader);
+
 		return trayMgtHeaderRes;
 	}
 
 	@Override
 	public TrayMgtDetailBean saveTrayMgtDetail(TrayMgtDetailBean trayMgtDetail) {
 
-		TrayMgtDetailBean trayMgtDetailsRes=trayMgtDetailBeanRepository.saveAndFlush(trayMgtDetail);
-		
+		try {
+			if (trayMgtDetail.getTrayStatus() == 0) {
+				trayMgtDetail.setTrayStatus(1);
+			}
+		} catch (Exception e) {
+		}
+
+		TrayMgtDetailBean trayMgtDetailsRes = trayMgtDetailBeanRepository.saveAndFlush(trayMgtDetail);
+
 		return trayMgtDetailsRes;
 	}
 
@@ -77,11 +83,9 @@ public class TrayMgtServiceImpl implements TrayMgtService{
 
 		List<FrOutTrays> frOutTrays;
 		try {
-			frOutTrays=frOutTraysRepository.findOutTrays(frId,billDate);
-		}
-		catch(Exception e)
-		{
-			frOutTrays=new ArrayList<>();
+			frOutTrays = frOutTraysRepository.findOutTrays(frId, billDate);
+		} catch (Exception e) {
+			frOutTrays = new ArrayList<>();
 			e.printStackTrace();
 		}
 		return frOutTrays;
@@ -90,15 +94,12 @@ public class TrayMgtServiceImpl implements TrayMgtService{
 	@Override
 	public Info updateExtraOutTrays(int tranId, String extraOutTrays) {
 
-		int isUpdated=trayMgtHeaderRepository.updateExtraOutTrays(tranId,extraOutTrays);
-		Info info=new Info();
-		if(isUpdated>=1)
-		{
+		int isUpdated = trayMgtHeaderRepository.updateExtraOutTrays(tranId, extraOutTrays);
+		Info info = new Info();
+		if (isUpdated >= 1) {
 			info.setError(false);
 			info.setMessage("TrayMgtHeader Updated Successfully.");
-		}
-		else
-		{
+		} else {
 			info.setError(true);
 			info.setMessage("TrayMgtHeader Updatation Failed.");
 		}
@@ -106,16 +107,14 @@ public class TrayMgtServiceImpl implements TrayMgtService{
 	}
 
 	@Override
-	public List<GetTrayMgtHeader> getLoadedVehicles(String date,int vehStatus) {
-
+	public List<GetTrayMgtHeader> getLoadedVehicles(String date, int vehStatus) {
 
 		List<GetTrayMgtHeader> trayMgtHeaders;
 		try {
-		
-			trayMgtHeaders=getTrayMgtHeaderRepository.getLoadedVehicles(date,vehStatus);
-		}
-		catch (Exception e) {
-			trayMgtHeaders=new ArrayList<GetTrayMgtHeader>();
+
+			trayMgtHeaders = getTrayMgtHeaderRepository.getLoadedVehicles(date, vehStatus);
+		} catch (Exception e) {
+			trayMgtHeaders = new ArrayList<GetTrayMgtHeader>();
 			e.printStackTrace();
 		}
 		return trayMgtHeaders;
@@ -124,17 +123,14 @@ public class TrayMgtServiceImpl implements TrayMgtService{
 	@Override
 	public Info updateVehicleOutData(int tranId, String vehOuttime, float vehOutKm) {
 
-		int isUpdated=trayMgtHeaderRepository.updateVehicleOutData(tranId,vehOuttime,vehOutKm);
-		int updateStatus=trayMgtDetailBeanRepository.updateTrayStatusByTranId(tranId,1);
-		
-		Info info=new Info();
-		if(isUpdated>=1&&updateStatus>=1)
-		{
+		int isUpdated = trayMgtHeaderRepository.updateVehicleOutData(tranId, vehOuttime, vehOutKm);
+		int updateStatus = trayMgtDetailBeanRepository.updateTrayStatusByTranId(tranId, 1);
+
+		Info info = new Info();
+		if (isUpdated >= 1 && updateStatus >= 1) {
 			info.setError(false);
 			info.setMessage("TrayMgtHeader Updated Successfully.");
-		}
-		else
-		{
+		} else {
 			info.setError(true);
 			info.setMessage("TrayMgtHeader Updatation Failed.");
 		}
@@ -144,15 +140,12 @@ public class TrayMgtServiceImpl implements TrayMgtService{
 	@Override
 	public Info updateInVehicleData(int tranId, String vehIntime, float vehInkm, float vehRunningkm) {
 
-		int isUpdated=trayMgtHeaderRepository.updateInVehicleData(tranId,vehIntime,vehInkm,vehRunningkm);
-		Info info=new Info();
-		if(isUpdated>=1)
-		{
+		int isUpdated = trayMgtHeaderRepository.updateInVehicleData(tranId, vehIntime, vehInkm, vehRunningkm);
+		Info info = new Info();
+		if (isUpdated >= 1) {
 			info.setError(false);
 			info.setMessage("TrayMgtHeader Updated Successfully.");
-		}
-		else
-		{
+		} else {
 			info.setError(true);
 			info.setMessage("TrayMgtHeader Updatation Failed.");
 		}
@@ -160,24 +153,22 @@ public class TrayMgtServiceImpl implements TrayMgtService{
 	}
 
 	@Override
-	public List<FranchiseInRoute> getFranchiseInRoute(int routeId,int tranId) {
+	public List<FranchiseInRoute> getFranchiseInRoute(int routeId, int tranId) {
 
 		List<FranchiseInRoute> frInRoute = null;
 		try {
-		 frInRoute=franchiseInRouteRepository.findFrInRoute(routeId,tranId);
-		}
-		catch(Exception e)
-		{
-			frInRoute=new ArrayList<FranchiseInRoute>();
+			frInRoute = franchiseInRouteRepository.findFrInRoute(routeId, tranId);
+		} catch (Exception e) {
+			frInRoute = new ArrayList<FranchiseInRoute>();
 			e.printStackTrace();
 		}
 		return frInRoute;
 	}
 
 	@Override
-	public TrayMgtDetail getTrayMgtDetailByFrId(int frId,int tranId) {
-		
-		TrayMgtDetail trayMgtDetailRes=trayMgtDetailRepository.findByFrIdAndTranIdAndDelStatus(frId,tranId,0);
+	public TrayMgtDetail getTrayMgtDetailByFrId(int frId, int tranId) {
+
+		TrayMgtDetail trayMgtDetailRes = trayMgtDetailRepository.findByFrIdAndTranIdAndDelStatus(frId, tranId, 0);
 		return trayMgtDetailRes;
 	}
 
@@ -186,11 +177,10 @@ public class TrayMgtServiceImpl implements TrayMgtService{
 
 		GetTrayMgtHeader trayMgtHeaderRes;
 		try {
-		
-			trayMgtHeaderRes=getTrayMgtHeaderRepository.getTrayMgtHeader(tranId);
-		}
-		catch (Exception e) {
-			trayMgtHeaderRes=new GetTrayMgtHeader();
+
+			trayMgtHeaderRes = getTrayMgtHeaderRepository.getTrayMgtHeader(tranId);
+		} catch (Exception e) {
+			trayMgtHeaderRes = new GetTrayMgtHeader();
 			e.printStackTrace();
 		}
 		return trayMgtHeaderRes;
@@ -198,15 +188,12 @@ public class TrayMgtServiceImpl implements TrayMgtService{
 
 	@Override
 	public Info updateDieselForTransaction(int tranId, float diesel) {
-		int isUpdated=trayMgtHeaderRepository.updateDieselForTransaction(tranId,diesel);
-		Info info=new Info();
-		if(isUpdated>=1)
-		{
+		int isUpdated = trayMgtHeaderRepository.updateDieselForTransaction(tranId, diesel);
+		Info info = new Info();
+		if (isUpdated >= 1) {
 			info.setError(false);
 			info.setMessage("TrayMgtHeader Updated Successfully.");
-		}
-		else
-		{
+		} else {
 			info.setError(true);
 			info.setMessage("TrayMgtHeader Updatation Failed.");
 		}
@@ -215,15 +202,12 @@ public class TrayMgtServiceImpl implements TrayMgtService{
 
 	@Override
 	public Info updateExtraInTrays(int tranId, String extraInTrays) {
-		int isUpdated=trayMgtHeaderRepository.updateExtraInTrays(tranId,extraInTrays);
-		Info info=new Info();
-		if(isUpdated>=1)
-		{
+		int isUpdated = trayMgtHeaderRepository.updateExtraInTrays(tranId, extraInTrays);
+		Info info = new Info();
+		if (isUpdated >= 1) {
 			info.setError(false);
 			info.setMessage("TrayMgtHeader Updated Successfully.");
-		}
-		else
-		{
+		} else {
 			info.setError(true);
 			info.setMessage("TrayMgtHeader Updatation Failed.");
 		}
@@ -234,43 +218,42 @@ public class TrayMgtServiceImpl implements TrayMgtService{
 	public TrayMgtDetail getTrayDetailByStatus(int frId, int trayStatus, int isSameDay) {
 		TrayMgtDetail trayMgtDetailRes;
 		try {
-		
-			trayMgtDetailRes=trayMgtDetailRepository.findByFrIdAndTrayStatusAndIsSameDayAndDelStatus(frId,trayStatus,isSameDay,0);
-		}
-		catch (Exception e) {
-			trayMgtDetailRes=new TrayMgtDetail();
+
+			trayMgtDetailRes = trayMgtDetailRepository.findByFrIdAndTrayStatusAndIsSameDayAndDelStatus(frId, trayStatus,
+					isSameDay, 0);
+		} catch (Exception e) {
+			trayMgtDetailRes = new TrayMgtDetail();
 			e.printStackTrace();
 		}
-		return trayMgtDetailRes;	
-		}
+		return trayMgtDetailRes;
+	}
 
 	@Override
 	public TrayMgtDetailBean getTrayDetailByDetailId(int tranStatus3) {
 		TrayMgtDetailBean trayMgtDetailRes;
 		try {
-		
-			trayMgtDetailRes=trayMgtDetailBeanRepository.findByTranDetailId(tranStatus3);
-		}
-		catch (Exception e) {
-			trayMgtDetailRes=new TrayMgtDetailBean();
+
+			trayMgtDetailRes = trayMgtDetailBeanRepository.findByTranDetailId(tranStatus3);
+		} catch (Exception e) {
+			trayMgtDetailRes = new TrayMgtDetailBean();
 			e.printStackTrace();
 		}
-		return trayMgtDetailRes;	
+		return trayMgtDetailRes;
 	}
 
 	@Override
-	public int updateTrayStatus(int tranStatus1,int status) {
+	public int updateTrayStatus(int tranStatus1, int status) {
 
-		int isUpdated=trayMgtDetailBeanRepository.updateTrayStatus(tranStatus1,status);
-		
+		int isUpdated = trayMgtDetailBeanRepository.updateTrayStatus(tranStatus1, status);
+
 		return isUpdated;
 	}
 
 	@Override
 	public TrayMgtHeader getTrayMgtHeaderByTranId(int tranId) {
 
-		TrayMgtHeader trayMgtHeaderRes=trayMgtHeaderRepository.findByTranId(tranId);
-		
+		TrayMgtHeader trayMgtHeaderRes = trayMgtHeaderRepository.findByTranId(tranId);
+
 		return trayMgtHeaderRes;
 	}
 
@@ -278,39 +261,37 @@ public class TrayMgtServiceImpl implements TrayMgtService{
 	public List<TrayMgtDetail> getTrayMgtDetailByTranId(int tranId) {
 		List<TrayMgtDetail> trayMgtDetailRes;
 		try {
-		
-			trayMgtDetailRes=trayMgtDetailRepository.findByTranId(tranId);
-		}
-		catch (Exception e) {
-			trayMgtDetailRes=new ArrayList<>();
+
+			trayMgtDetailRes = trayMgtDetailRepository.findByTranId(tranId);
+		} catch (Exception e) {
+			trayMgtDetailRes = new ArrayList<>();
 			e.printStackTrace();
 		}
-		return trayMgtDetailRes;	
+		return trayMgtDetailRes;
 	}
 
 	@Override
 	public List<TrayMgtDetail> getTrayDetailForTrayIn(int frId, int isSameDay) {
 		List<TrayMgtDetail> trayMgtDetailRes;
 		try {
-		
-			trayMgtDetailRes=trayMgtDetailRepository.findByFrIdAndIsSameDayAndDelStatusAndTrayStatusIn(frId,isSameDay,0);
-		}
-		catch (Exception e) {
-			trayMgtDetailRes=new ArrayList<>();
+
+			trayMgtDetailRes = trayMgtDetailRepository.findByFrIdAndIsSameDayAndDelStatusAndTrayStatusIn(frId,
+					isSameDay, 0);
+		} catch (Exception e) {
+			trayMgtDetailRes = new ArrayList<>();
 			e.printStackTrace();
 		}
-		return trayMgtDetailRes;	
+		return trayMgtDetailRes;
 	}
 
 	@Override
 	public List<GetTrayMgtHeader> getAllVehicles(String date) {
 		List<GetTrayMgtHeader> trayMgtHeaders;
 		try {
-		
-			trayMgtHeaders=getTrayMgtHeaderRepository.getAllVehicles(date);
-		}
-		catch (Exception e) {
-			trayMgtHeaders=new ArrayList<GetTrayMgtHeader>();
+
+			trayMgtHeaders = getTrayMgtHeaderRepository.getAllVehicles(date);
+		} catch (Exception e) {
+			trayMgtHeaders = new ArrayList<GetTrayMgtHeader>();
 			e.printStackTrace();
 		}
 		return trayMgtHeaders;
@@ -320,74 +301,67 @@ public class TrayMgtServiceImpl implements TrayMgtService{
 	public List<TrayMgtDetail> getTrayMgtDetailsForBill() {
 		List<TrayMgtDetail> trayMgtDetailRes;
 		try {
-		
-			trayMgtDetailRes=trayMgtDetailRepository.getTrayMgtDetailsForBill();
-		}
-		catch (Exception e) {
-			trayMgtDetailRes=new ArrayList<>();
+
+			trayMgtDetailRes = trayMgtDetailRepository.getTrayMgtDetailsForBill();
+		} catch (Exception e) {
+			trayMgtDetailRes = new ArrayList<>();
 			e.printStackTrace();
 		}
-		return trayMgtDetailRes;	
+		return trayMgtDetailRes;
 	}
 
 	@Override
 	public List<TrayMgtDetailBean> saveTrayMgtDetailForBill(List<TrayMgtDetailBean> trayMgtDetails) {
 		List<TrayMgtDetailBean> trayMgtDetailRes;
 		try {
-		
-			trayMgtDetailRes=trayMgtDetailBeanRepository.save(trayMgtDetails);
-		}
-		catch (Exception e) {
-			trayMgtDetailRes=new ArrayList<>();
+
+			trayMgtDetailRes = trayMgtDetailBeanRepository.save(trayMgtDetails);
+		} catch (Exception e) {
+			trayMgtDetailRes = new ArrayList<>();
 			e.printStackTrace();
 		}
-		return trayMgtDetailRes;	
+		return trayMgtDetailRes;
 	}
 
 	@Override
 	public List<TrayMgtDetail> getTrayMgtBillDetails(String fromDate, String toDate, List<String> frIds) {
 		List<TrayMgtDetail> trayMgtDetailRes;
 		try {
-		    if(frIds.contains("-1"))
-		    {
-		    	trayMgtDetailRes=trayMgtDetailRepository.findTrayMgtBillDetailsByDate(fromDate,toDate);
+			if (frIds.contains("-1")) {
+				trayMgtDetailRes = trayMgtDetailRepository.findTrayMgtBillDetailsByDate(fromDate, toDate);
 
-		    }else {
-			
-		    	trayMgtDetailRes=trayMgtDetailRepository.findTrayMgtBillDetailsByDateAndFr(fromDate,toDate,frIds);
-		    }
-		}
-		catch (Exception e) {
-			trayMgtDetailRes=new ArrayList<>();
+			} else {
+
+				trayMgtDetailRes = trayMgtDetailRepository.findTrayMgtBillDetailsByDateAndFr(fromDate, toDate, frIds);
+			}
+		} catch (Exception e) {
+			trayMgtDetailRes = new ArrayList<>();
 			e.printStackTrace();
 		}
-		return trayMgtDetailRes;	
+		return trayMgtDetailRes;
 	}
 
 	@Override
 	public ErrorMessage deleteTrayMgtHeader(int tranId) {
-		
-		ErrorMessage errorMessage=new ErrorMessage();
-	try {
-          int updateStatus=trayMgtHeaderRepository.updateHeaderStatus(tranId);
-      
-          int updateDetailStatus=trayMgtDetailBeanRepository.updateDelStatus(tranId);
-          
-          if(updateStatus>0||updateDetailStatus>0)
-          {
-        	  errorMessage=new ErrorMessage();
-        	  errorMessage.setError(false);
-        	  errorMessage.setMessage("Header with Details Deleted Successfully.");
-          }
-	  }
-	catch(Exception e)
-	{
-		 errorMessage=new ErrorMessage();
-    	 errorMessage.setError(false);
-   	     errorMessage.setMessage("Header with Details Deletion Successfully.");
-		e.printStackTrace();
-		
-	}
+
+		ErrorMessage errorMessage = new ErrorMessage();
+		try {
+			int updateStatus = trayMgtHeaderRepository.updateHeaderStatus(tranId);
+
+			int updateDetailStatus = trayMgtDetailBeanRepository.updateDelStatus(tranId);
+
+			if (updateStatus > 0 || updateDetailStatus > 0) {
+				errorMessage = new ErrorMessage();
+				errorMessage.setError(false);
+				errorMessage.setMessage("Header with Details Deleted Successfully.");
+			}
+		} catch (Exception e) {
+			errorMessage = new ErrorMessage();
+			errorMessage.setError(false);
+			errorMessage.setMessage("Header with Details Deletion Successfully.");
+			e.printStackTrace();
+
+		}
 		return errorMessage;
 	}
 
@@ -396,11 +370,10 @@ public class TrayMgtServiceImpl implements TrayMgtService{
 
 		List<GetTrayMgtHeader> trayMgtHeaders;
 		try {
-		
-			trayMgtHeaders=getTrayMgtHeaderRepository.getLoadedVehiclesByStatus(vehStatus);
-		}
-		catch (Exception e) {
-			trayMgtHeaders=new ArrayList<GetTrayMgtHeader>();
+
+			trayMgtHeaders = getTrayMgtHeaderRepository.getLoadedVehiclesByStatus(vehStatus);
+		} catch (Exception e) {
+			trayMgtHeaders = new ArrayList<GetTrayMgtHeader>();
 			e.printStackTrace();
 		}
 		return trayMgtHeaders;
@@ -410,76 +383,76 @@ public class TrayMgtServiceImpl implements TrayMgtService{
 	public List<TrayMgtDetail> getTrayMgtDetailList(String fromDate, String toDate, int frId, int isDepositUsed) {
 		List<TrayMgtDetail> trayMgtDetailRes;
 		try {
-		    if(isDepositUsed<=1)
-		    {
-		    	trayMgtDetailRes=trayMgtDetailRepository.findAllTrayMgtBillDetails(fromDate,toDate,frId,isDepositUsed);
+			if (isDepositUsed <= 1) {
+				trayMgtDetailRes = trayMgtDetailRepository.findAllTrayMgtBillDetails(fromDate, toDate, frId,
+						isDepositUsed);
 
-		    }else {
-			
-		    	trayMgtDetailRes=trayMgtDetailRepository.findTrayMgtBillDetails(fromDate,toDate,frId);
-		    }
-		}
-		catch (Exception e) {
-			trayMgtDetailRes=new ArrayList<>();
+			} else {
+
+				trayMgtDetailRes = trayMgtDetailRepository.findTrayMgtBillDetails(fromDate, toDate, frId);
+			}
+		} catch (Exception e) {
+			trayMgtDetailRes = new ArrayList<>();
 			e.printStackTrace();
 		}
-		return trayMgtDetailRes;	
+		return trayMgtDetailRes;
 	}
 
 	@Override
-	public List<TrayMgtDetailList> getTrayMgtDetailsByTranIdAndDate(int tranId,String date) {
+	public List<TrayMgtDetailList> getTrayMgtDetailsByTranIdAndDate(int tranId, String date) {
 		List<TrayMgtDetail> trayMgtDetailRes;
-		List<TrayMgtDetailList> trayMgtDetailList=new ArrayList<TrayMgtDetailList>();
+		List<TrayMgtDetailList> trayMgtDetailList = new ArrayList<TrayMgtDetailList>();
 		try {
-		        // String date=(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
-			trayMgtDetailRes=trayMgtDetailRepository.findByTranId(tranId);
-			for(int i=0;i<trayMgtDetailRes.size();i++)
-			{
-				TrayMgtDetailList trayMgtDetail=new TrayMgtDetailList();
-				
-				List<TrayMgtDetail> trayDetailList=new ArrayList<TrayMgtDetail>();
+			// String date=(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+			trayMgtDetailRes = trayMgtDetailRepository.findByTranId(tranId);
+			for (int i = 0; i < trayMgtDetailRes.size(); i++) {
+				TrayMgtDetailList trayMgtDetail = new TrayMgtDetailList();
+
+				List<TrayMgtDetail> trayDetailList = new ArrayList<TrayMgtDetail>();
 				trayDetailList.add(trayMgtDetailRes.get(i));
-				
+
 				List<TrayMgtDetail> trayMgtDetails = trayMgtDetailRepository
 						.findByIntrayDate(trayMgtDetailRes.get(i).getFrId(), date);
 				trayDetailList.addAll(trayMgtDetails);
-				
+
 				trayMgtDetail.setFrId(trayMgtDetailRes.get(i).getFrId());
 				trayMgtDetail.setTrayMgtDetailsList(trayDetailList);
-				
+
 				trayMgtDetailList.add(trayMgtDetail);
 				System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
 			}
-		}
-		catch (Exception e) {
-			trayMgtDetailList=new ArrayList<TrayMgtDetailList>();
+		} catch (Exception e) {
+			trayMgtDetailList = new ArrayList<TrayMgtDetailList>();
 			e.printStackTrace();
 		}
-		return trayMgtDetailList;	
+		return trayMgtDetailList;
 	}
-	
-	
+
 	@Override
 	public List<GetTrayMgtReport> getTrayMgtBalanceTrayList(int tranId, List<Integer> frIdList) {
-		List<GetTrayMgtReport> getTrayMgtBalanceTrayList= getTrayMgtReportRepo.getTrayMgtBalanceTrayList(tranId, frIdList);;
+		List<GetTrayMgtReport> getTrayMgtBalanceTrayList = getTrayMgtReportRepo.getTrayMgtBalanceTrayList(tranId,
+				frIdList);
+		;
 
 		return getTrayMgtBalanceTrayList;
 	}
-	
+
 	@Override
 	public List<GetInTrays> getTrayMgtInTrayList(int tranId, List<Integer> frIdList) {
-		List<GetInTrays> getTrayMgtInTrayList= getInTraysRepository.getTrayMgtInTrayList(tranId, frIdList);;
+		List<GetInTrays> getTrayMgtInTrayList = getInTraysRepository.getTrayMgtInTrayList(tranId, frIdList);
+		;
 
 		return getTrayMgtInTrayList;
 	}
 
-	
 	@Override
 	public List<TrayMgtDetail> getTrayDetailForBalanceByFr(int frId) {
 		List<TrayMgtDetail> trayMgtDetailRes;
 		try {
 
-			trayMgtDetailRes = trayMgtDetailRepository.findByFrIdAndDelStatus(frId, 0);
+			// trayMgtDetailRes = trayMgtDetailRepository.findByFrIdAndDelStatus(frId, 0);
+			trayMgtDetailRes = trayMgtDetailRepository.getBalByFr(frId);
+
 		} catch (Exception e) {
 			trayMgtDetailRes = new ArrayList<>();
 			e.printStackTrace();
